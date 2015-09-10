1890e66120a384df2fbe9b8ffbac751a4960827c
Arran Huxtable <arran.huxtable@adp.com>
2015-09-10 17:29:39 +0100

    We can now specifically ask for devDependencies, dependencies or both. A little more logging....

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 'use strict';
 
-module.exports = function(packageFile, whichdependencies) {
+module.exports = function(packageFile, whichDependencies) {
   var fs = require('fs');
   var packageJson = require(packageFile);
-  var dependencies = whichdependencies || 'dependencies';
 
-  if(packageJson[dependencies]) {
-    packageJson.bundledDependencies = Object.keys(packageJson[dependencies]);
+  var dependenciesAskedFor = whichDependencies || 'dependencies';
+  var dependenciesToUse = [];
+  var productionDependenciesKey = 'dependencies';
+  var devDependenciesKey = 'devDependencies';
+  var foundAnyToAdd = false;
 
+  var bothDependencies = dependenciesAskedFor === "both";
+  if (bothDependencies) {
+    dependenciesToUse.push(productionDependenciesKey);
+    dependenciesToUse.push(devDependenciesKey);
+  } else {
+    dependenciesToUse.push(dependenciesAskedFor);
+  }
+
+  console.log("Going to pick bundled dependencies from: " + dependenciesToUse.join(", "));
+
+  packageJson.bundledDependencies = [];
+  dependenciesToUse.forEach(function (dependencySection) {
+    if (packageJson[dependencySection]) {
+      foundAnyToAdd = true;
+      var dependenciesFoundToAdd = Object.keys(packageJson[dependencySection]);
+      dependenciesFoundToAdd.forEach(function(dependency) {
+        packageJson.bundledDependencies.push(dependency);
+      });
+      console.log("bundledDependencies section updated with the following dependencies: ");
+      console.log(dependenciesFoundToAdd);
+    } else {
+      console.log("No dependencies found in the " + dependencySection + " section of the package.json");
+    }
+  });
+
+  if (foundAnyToAdd) {
+    // only update it if we have anything to update.
     fs.writeFile(packageFile, JSON.stringify(packageJson, null, 4), function(error) {
       if (error) {
         throw error;
       }
-      console.log('Package.json updated with the following bundled dependencies:');
-      console.log(packageJson.bundledDependencies);
+      console.log("Successfully updated package.json with bundled dependencies.");
     });
-  } else {
-    console.log('No dependencies found');
+  }
+  else {
+    console.log("Didn't find any bundled dependencies so we haven't touched package.json.");
   }
 };
